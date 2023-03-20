00f1b3720d65bc28a18c06f95f9523ba4904fefe
ogorodtrue <semenenkogleb195@gmail.com>
2023-03-21 00:14:27 +0300

    end

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,13 @@
 package app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Timer;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
 }
